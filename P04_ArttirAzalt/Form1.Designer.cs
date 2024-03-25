aca4cc3154dd37791f73ca03a459dc0fcab0a35f
Arı Bilgi <nomail@aribilgi.com.tr>
2024-03-25 11:25:03 +0300

    Some changes

--- P04_ArttirAzalt/Form1.Designer.cs
+++ P04_ArttirAzalt/Form1.Designer.cs
@@ -34,6 +34,8 @@
             this.rbtnAzalt = new System.Windows.Forms.RadioButton();
             this.btonIslem = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,7 +54,7 @@
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(95, 37);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(81, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(61, 20);
             this.numericUpDown1.TabIndex = 1;
             // 
             // rbtnArttir
@@ -85,22 +87,44 @@
             this.btonIslem.TabIndex = 4;
             this.btonIslem.Text = "İşlem Yap";
             this.btonIslem.UseVisualStyleBackColor = true;
+            this.btonIslem.Click += new System.EventHandler(this.btonIslem_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(92, 213);
+            this.label1.Location = new System.Drawing.Point(27, 206);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 31);
+            this.label1.Size = new System.Drawing.Size(113, 31);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Sonuc";
+            this.label1.Text = "Sonuc : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Miktar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(141, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 31);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(276, 351);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btonIslem);
             this.Controls.Add(this.rbtnAzalt);
@@ -109,6 +133,7 @@
             this.Controls.Add(this.btonExit);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -123,6 +148,8 @@
         private System.Windows.Forms.RadioButton rbtnAzalt;
         private System.Windows.Forms.Button btonIslem;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- P04_ArttirAzalt/Form1.cs
+++ P04_ArttirAzalt/Form1.cs
@@ -21,5 +21,45 @@ namespace P04_ArttirAzalt
         {
             Application.Exit();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numericUpDown1.Maximum = 1000;
+
+        }
+
+        private void btonIslem_Click(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value > numericUpDown1.Maximum)
+            {
+                MessageBox.Show("Lütfen maksimum değerimi aşmayınız...");
+            }
+
+
+
+
+
+            if (rbtnArttir.Checked == true) // arttır butonu seçilmişse
+            {
+                int miktar = Convert.ToInt32(numericUpDown1.Value); // ne kadarlık bir işlem yapacağım
+
+                int sonuc = Convert.ToInt32(label3.Text);  
+
+                label3.Text= (sonuc + miktar).ToString();
+            }
+            else if (rbtnAzalt.Checked==true) // azalt butonu
+            {
+                int miktar = Convert.ToInt32(numericUpDown1.Value); // ne kadarlık bir işlem yapacağım
+
+                int sonuc = Convert.ToInt32(label3.Text);
+
+                label3.Text = (sonuc - miktar).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen ne yapacağımı belirtiniz...");
+            }
+
+        }
     }
 }
