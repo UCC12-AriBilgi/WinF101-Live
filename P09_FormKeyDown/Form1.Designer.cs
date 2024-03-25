69843d7cb2e81b96f111eb5dfcefc412f28858ee
Arı Bilgi <nomail@aribilgi.com.tr>
2024-03-25 15:01:58 +0300

    Some changes

---
+++ P09_FormKeyDown/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace P09_FormKeyDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btonHareketli = new System.Windows.Forms.Button();
+            this.btonA = new System.Windows.Forms.Button();
+            this.btonS = new System.Windows.Forms.Button();
+            this.btonD = new System.Windows.Forms.Button();
+            this.btonW = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btonHareketli
+            // 
+            this.btonHareketli.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btonHareketli.Location = new System.Drawing.Point(185, 138);
+            this.btonHareketli.Name = "btonHareketli";
+            this.btonHareketli.Size = new System.Drawing.Size(52, 54);
+            this.btonHareketli.TabIndex = 0;
+            this.btonHareketli.UseVisualStyleBackColor = false;
+            // 
+            // btonA
+            // 
+            this.btonA.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btonA.Location = new System.Drawing.Point(21, 377);
+            this.btonA.Name = "btonA";
+            this.btonA.Size = new System.Drawing.Size(43, 43);
+            this.btonA.TabIndex = 1;
+            this.btonA.Text = "A";
+            this.btonA.UseVisualStyleBackColor = true;
+            this.btonA.Click += new System.EventHandler(this.btonA_Click);
+            // 
+            // btonS
+            // 
+            this.btonS.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btonS.Location = new System.Drawing.Point(70, 377);
+            this.btonS.Name = "btonS";
+            this.btonS.Size = new System.Drawing.Size(43, 43);
+            this.btonS.TabIndex = 2;
+            this.btonS.Text = "S";
+            this.btonS.UseVisualStyleBackColor = true;
+            this.btonS.Click += new System.EventHandler(this.btonS_Click);
+            // 
+            // btonD
+            // 
+            this.btonD.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btonD.Location = new System.Drawing.Point(119, 377);
+            this.btonD.Name = "btonD";
+            this.btonD.Size = new System.Drawing.Size(43, 43);
+            this.btonD.TabIndex = 3;
+            this.btonD.Text = "D";
+            this.btonD.UseVisualStyleBackColor = true;
+            this.btonD.Click += new System.EventHandler(this.btonD_Click);
+            // 
+            // btonW
+            // 
+            this.btonW.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btonW.Location = new System.Drawing.Point(70, 328);
+            this.btonW.Name = "btonW";
+            this.btonW.Size = new System.Drawing.Size(43, 43);
+            this.btonW.TabIndex = 4;
+            this.btonW.Text = "W";
+            this.btonW.UseVisualStyleBackColor = true;
+            this.btonW.Click += new System.EventHandler(this.btonW_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btonW);
+            this.Controls.Add(this.btonD);
+            this.Controls.Add(this.btonS);
+            this.Controls.Add(this.btonA);
+            this.Controls.Add(this.btonHareketli);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btonHareketli;
+        private System.Windows.Forms.Button btonA;
+        private System.Windows.Forms.Button btonS;
+        private System.Windows.Forms.Button btonD;
+        private System.Windows.Forms.Button btonW;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ P09_FormKeyDown/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P09_FormKeyDown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btonD_Click(object sender, EventArgs e)
+        {
+            Form1_KeyDown(sender, new KeyEventArgs(Keys.D));
+        }
+
+        private void btonA_Click(object sender, EventArgs e)
+        {
+            Form1_KeyDown(sender, new KeyEventArgs(Keys.A));
+        }
+
+        private void btonW_Click(object sender, EventArgs e)
+        {
+            Form1_KeyDown(sender, new KeyEventArgs(Keys.W));
+        }
+
+        private void btonS_Click(object sender, EventArgs e)
+        {
+            //int yeniX = btonHareketli.Location.X;
+            //int yeniY = btonHareketli.Location.Y + 5;
+            //int butonwidth = btonHareketli.Width;
+
+
+            //// butonun formdan kaybolmasını engelemek için
+
+            //if (yeniY < this.Height - butonwidth)
+            //{
+            //    btonHareketli.Location = new Point(yeniX, yeniY);
+            //}
+
+            Form1_KeyDown(sender, new KeyEventArgs(Keys.S));
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // hangi tuşa basarak acaba buraya geldim.
+
+            if (e.KeyCode == Keys.D) // D tusuna basılmış
+            {
+                int yeniX = btonHareketli.Location.X + 5;
+                int yeniY = btonHareketli.Location.Y;
+                int butonwidth = btonHareketli.Width;
+
+
+                // butonun formdan kaybolmasını engelemek için
+
+                if (yeniX < this.Width - butonwidth)
+                {
+                    btonHareketli.Location = new Point(yeniX, yeniY);
+                }
+            }
+
+            if (e.KeyCode == Keys.W) 
+            {
+                int yeniX = btonHareketli.Location.X;
+                int yeniY = btonHareketli.Location.Y - 5;
+                int butonwidth = btonHareketli.Width;
+
+                if (yeniY >= 0) // formun en baş sol tarafı
+                {
+                    btonHareketli.Location = new Point(yeniX, yeniY);
+                }
+            }
+
+            if (e.KeyCode==Keys.A)
+            {
+                int yeniX = btonHareketli.Location.X - 5;
+                int yeniY = btonHareketli.Location.Y;
+                int butonwidth = btonHareketli.Width;
+
+                if (yeniX >= 0) // formun en baş sol tarafı
+                {
+                    btonHareketli.Location = new Point(yeniX, yeniY);
+                }
+            }
+
+            if (e.KeyCode == Keys.S) 
+            {
+                int yeniX = btonHareketli.Location.X;
+                int yeniY = btonHareketli.Location.Y + 5;
+                int butonwidth = btonHareketli.Width;
+
+
+                // butonun formdan kaybolmasını engelemek için
+
+                if (yeniY < this.Height - butonwidth)
+                {
+                    btonHareketli.Location = new Point(yeniX, yeniY);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Onay())
+            {
+                Application.Exit();
+            }
+        }
+
+        private bool Onay()
+        {
+            bool secim = false;
+
+            DialogResult dialogResult= MessageBox.Show("Programdan çıkmak istiyor musunuz?","Onay",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+
+            if (dialogResult== DialogResult.Yes)
+            {
+                secim = true;
+            }
+            else if (dialogResult== DialogResult.No)
+            {
+                secim = false;
+            }
+
+            return secim;
+
+        }
+    }
+}
