5445c5a0cff6d460461dc869fd0fd944be2f1226
Arı Bilgi <nomail@aribilgi.com.tr>
2024-03-26 10:21:46 +0300

    Some changes

---
+++ P10_HesMak/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace P10_HesMak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tboxHistory = new System.Windows.Forms.TextBox();
+            this.tboxSonuc = new System.Windows.Forms.TextBox();
+            this.button0 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.btonEsittir = new System.Windows.Forms.Button();
+            this.btonTopla = new System.Windows.Forms.Button();
+            this.btonCikart = new System.Windows.Forms.Button();
+            this.btonCarp = new System.Windows.Forms.Button();
+            this.btonBol = new System.Windows.Forms.Button();
+            this.btonCE = new System.Windows.Forms.Button();
+            this.btonC = new System.Windows.Forms.Button();
+            this.btonHane = new System.Windows.Forms.Button();
+            this.btonIsaret = new System.Windows.Forms.Button();
+            this.btonNokta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tboxHistory
+            // 
+            this.tboxHistory.BackColor = System.Drawing.SystemColors.Control;
+            this.tboxHistory.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tboxHistory.Location = new System.Drawing.Point(12, 12);
+            this.tboxHistory.Name = "tboxHistory";
+            this.tboxHistory.Size = new System.Drawing.Size(190, 20);
+            this.tboxHistory.TabIndex = 0;
+            // 
+            // tboxSonuc
+            // 
+            this.tboxSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tboxSonuc.Location = new System.Drawing.Point(12, 38);
+            this.tboxSonuc.Name = "tboxSonuc";
+            this.tboxSonuc.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tboxSonuc.Size = new System.Drawing.Size(190, 38);
+            this.tboxSonuc.TabIndex = 1;
+            this.tboxSonuc.Text = "0";
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(61, 278);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(43, 43);
+            this.button0.TabIndex = 2;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(61, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(43, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(110, 229);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(43, 43);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 180);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(43, 43);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(61, 180);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(43, 43);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(110, 180);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(43, 43);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 131);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(43, 43);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(61, 131);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(43, 43);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(110, 131);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(43, 43);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // btonEsittir
+            // 
+            this.btonEsittir.Location = new System.Drawing.Point(159, 278);
+            this.btonEsittir.Name = "btonEsittir";
+            this.btonEsittir.Size = new System.Drawing.Size(43, 43);
+            this.btonEsittir.TabIndex = 12;
+            this.btonEsittir.Text = "=";
+            this.btonEsittir.UseVisualStyleBackColor = true;
+            // 
+            // btonTopla
+            // 
+            this.btonTopla.Location = new System.Drawing.Point(159, 229);
+            this.btonTopla.Name = "btonTopla";
+            this.btonTopla.Size = new System.Drawing.Size(43, 43);
+            this.btonTopla.TabIndex = 13;
+            this.btonTopla.Text = "+";
+            this.btonTopla.UseVisualStyleBackColor = true;
+            // 
+            // btonCikart
+            // 
+            this.btonCikart.Location = new System.Drawing.Point(159, 180);
+            this.btonCikart.Name = "btonCikart";
+            this.btonCikart.Size = new System.Drawing.Size(43, 43);
+            this.btonCikart.TabIndex = 14;
+            this.btonCikart.Text = "-";
+            this.btonCikart.UseVisualStyleBackColor = true;
+            // 
+            // btonCarp
+            // 
+            this.btonCarp.Location = new System.Drawing.Point(159, 131);
+            this.btonCarp.Name = "btonCarp";
+            this.btonCarp.Size = new System.Drawing.Size(43, 43);
+            this.btonCarp.TabIndex = 15;
+            this.btonCarp.Text = "*";
+            this.btonCarp.UseVisualStyleBackColor = true;
+            // 
+            // btonBol
+            // 
+            this.btonBol.Location = new System.Drawing.Point(159, 82);
+            this.btonBol.Name = "btonBol";
+            this.btonBol.Size = new System.Drawing.Size(43, 43);
+            this.btonBol.TabIndex = 16;
+            this.btonBol.Text = "/";
+            this.btonBol.UseVisualStyleBackColor = true;
+            // 
+            // btonCE
+            // 
+            this.btonCE.Location = new System.Drawing.Point(12, 82);
+            this.btonCE.Name = "btonCE";
+            this.btonCE.Size = new System.Drawing.Size(43, 43);
+            this.btonCE.TabIndex = 17;
+            this.btonCE.Text = "CE";
+            this.btonCE.UseVisualStyleBackColor = true;
+            // 
+            // btonC
+            // 
+            this.btonC.Location = new System.Drawing.Point(61, 82);
+            this.btonC.Name = "btonC";
+            this.btonC.Size = new System.Drawing.Size(43, 43);
+            this.btonC.TabIndex = 18;
+            this.btonC.Text = "C";
+            this.btonC.UseVisualStyleBackColor = true;
+            // 
+            // btonHane
+            // 
+            this.btonHane.Location = new System.Drawing.Point(110, 82);
+            this.btonHane.Name = "btonHane";
+            this.btonHane.Size = new System.Drawing.Size(43, 43);
+            this.btonHane.TabIndex = 19;
+            this.btonHane.Text = "<<";
+            this.btonHane.UseVisualStyleBackColor = true;
+            // 
+            // btonIsaret
+            // 
+            this.btonIsaret.Location = new System.Drawing.Point(12, 278);
+            this.btonIsaret.Name = "btonIsaret";
+            this.btonIsaret.Size = new System.Drawing.Size(43, 43);
+            this.btonIsaret.TabIndex = 20;
+            this.btonIsaret.Text = "+/-";
+            this.btonIsaret.UseVisualStyleBackColor = true;
+            // 
+            // btonNokta
+            // 
+            this.btonNokta.Location = new System.Drawing.Point(110, 278);
+            this.btonNokta.Name = "btonNokta";
+            this.btonNokta.Size = new System.Drawing.Size(43, 43);
+            this.btonNokta.TabIndex = 21;
+            this.btonNokta.Text = ".";
+            this.btonNokta.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(215, 340);
+            this.Controls.Add(this.btonNokta);
+            this.Controls.Add(this.btonIsaret);
+            this.Controls.Add(this.btonHane);
+            this.Controls.Add(this.btonC);
+            this.Controls.Add(this.btonCE);
+            this.Controls.Add(this.btonBol);
+            this.Controls.Add(this.btonCarp);
+            this.Controls.Add(this.btonCikart);
+            this.Controls.Add(this.btonTopla);
+            this.Controls.Add(this.btonEsittir);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.tboxSonuc);
+            this.Controls.Add(this.tboxHistory);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tboxHistory;
+        private System.Windows.Forms.TextBox tboxSonuc;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btonEsittir;
+        private System.Windows.Forms.Button btonTopla;
+        private System.Windows.Forms.Button btonCikart;
+        private System.Windows.Forms.Button btonCarp;
+        private System.Windows.Forms.Button btonBol;
+        private System.Windows.Forms.Button btonCE;
+        private System.Windows.Forms.Button btonC;
+        private System.Windows.Forms.Button btonHane;
+        private System.Windows.Forms.Button btonIsaret;
+        private System.Windows.Forms.Button btonNokta;
+    }
+}
+
